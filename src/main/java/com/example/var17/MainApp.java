834be6a5521891f68743305df8615ffb173aa16b
Aiza <aiza.tugolbaeva@alatoo.edu.kg>
2025-05-21 13:02:08 +0600

    Commit

--- src/main/java/com/example/var17/MainApp.java
+++ src/main/java/com/example/var17/MainApp.java
@@ -2,14 +2,22 @@ package com.example.var17;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("pvz_view.fxml"));
-        Scene scene = new Scene(loader.load());
+        URL fxmlUrl = getClass().getResource("/com/example/var17/pvz_view.fxml");
+        System.out.println("FXML path: " + fxmlUrl);
+
+        FXMLLoader loader = new FXMLLoader(fxmlUrl);
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
         stage.setTitle("Фильтр и визуализация ПВЗ");
         stage.setScene(scene);
         stage.show();
